80b4358ab9f90d4c18972e871b618fdbcd7196d8
Steve Steele <steven.harville.steele@gmail.com>
2022-07-13 12:36:42 -0500

    Add Entry model

---
+++ app/Models/Entry.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Entry extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'date',
+        'tempo',
+        'entry',
+    ];
+
+    // /**
+    //  * The attributes excluded from the model's JSON form.
+    //  *
+    //  * @var array
+    //  */
+    // protected $dates = ['date'];
+
+    // /**
+    //  * The attributes that should be hidden for serialization.
+    //  *
+    //  * @var array<int, string>
+    //  */
+    // protected $hidden = [
+    //     'date',
+    // ];
+
+    /**
+     * Define the relationship between two eloquent models: Entry & User
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    // /**
+    //  * Define the relationship between two eloquent models: Entry & EntryHasTag
+    //  */
+    // public function entryHasTag()
+    // {
+    //     return $this->hasMany('App\EntryHasTag');
+    // }
+
+    // /**
+    //  * Define the relationship between two eloquent models: Entry & EntryHasTag
+    //  */
+    // public function entryHasMention()
+    // {
+    //     return $this->hasMany('App\EntryHasMention');
+    // }
+
+    // /**
+    //  * Define the relationship between two eloquent models: Entry & Marker
+    //  */
+    // public function marker()
+    // {
+    //     return $this->hasMany('App\Marker');
+    // }
+}
