c5afa3ee01a083df88bfc819acd83792b61ad8a0
Steve Steele <steven.harville.steele@gmail.com>
2022-07-22 12:19:02 -0500

    Use system font

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Avenir', ...defaultTheme.fontFamily.sans],
             },
         },
     },
