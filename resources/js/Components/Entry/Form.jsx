b40456c14ae17c7fce2d7199a62954325c23ed7a
Steve Steele <steven.harville.steele@gmail.com>
2022-07-22 14:15:14 -0500

    Extract annotation logic to form component
    ...and employ in Create and Edit pages

---
+++ resources/js/Components/Entry/Form.jsx
@@ -0,0 +1,273 @@
+import AutoAnnotation from '@/Components/AutoAnnotation';
+import UseFocus from '@/Utils/UseFocus';
+import { useForm } from '@inertiajs/inertia-react';
+import React, { useEffect, useState } from 'react';
+
+export default function Form({ dbEntry = {}, mentions, tags }) {
+    const defaultDate = new Date().toISOString().slice(0, 10);
+    const {
+        id,
+        date = defaultDate,
+        tempo = '',
+        entry = '',
+    } = dbEntry;
+    const isExistingEntry = !!id;
+    const initialState = {
+        date,
+        tempo,
+        entry,
+    };
+    const { data, errors: formErrors, post, put, setData } = useForm(initialState);
+    const [annotationStartIndex, setAnnotationStartIndex] = useState(0);
+    const [isAnnotatingStart, setIsAnnotatingStart] = useState(false);
+    const [isAnnotating, setIsAnnotating] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [suggestedAnnotations, setSuggestedAnnotations] = useState([]);
+    const [reset, setReset] = useState(null);
+    const [inputRef, setInputFocus] = UseFocus();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (isExistingEntry) {
+            put(route('entries.update', id), {
+                onSuccess: () => {
+                    // @todo: flash notify
+                    console.log('Entry updated');
+                },
+            });
+        } else {
+            post(route('entries.store'), {
+                onSuccess: () => {
+                    // @todo: flash notify
+                    console.log('Entry added');
+                },
+            });
+        }
+    }
+
+    function getAnnotationState() {
+        return {
+            annotationStartIndex,
+            entry: data?.entry,
+            searchTerm,
+            suggestedAnnotations,
+        }
+    }
+
+    useEffect(() => {
+        suggestAnnotations(searchTerm);
+    }, [searchTerm]);
+
+    useEffect(() => {
+        if (isAnnotatingStart) {
+            setReset(null);
+            setIsAnnotatingStart(false);
+            const { entry } = getAnnotationState();
+            let regex;
+            if ('tag' === isAnnotating) {
+                regex = /(?<=\s|^)#\w(?=\s|$)/
+            } else if ('mention' === isAnnotating) {
+                regex = /(?<=\s|^)@\w(?=\s|$)/
+            }
+            const annotationStartIndex = entry.search(regex) + 1;
+            setAnnotationStartIndex(annotationStartIndex);
+        }
+    }, [data?.entry]);
+
+    useEffect(() => {
+        if (null !== reset) {
+            setInputFocus(reset);
+            setAnnotationStartIndex(0);
+            setIsAnnotating(false);
+            setSearchTerm('');
+            setSuggestedAnnotations([]);
+        }
+    }, [reset]);
+
+    function populateAnnotation({
+        annotationStartIndex,
+        entry,
+        searchTerm,
+        text,
+    }) {
+        const pre = entry.substring(0, annotationStartIndex);
+        const post = entry.substring(annotationStartIndex + searchTerm.length);
+        entry = `${pre}${text} ${post}`;
+        setData('entry', entry);
+
+        const annotationEndIndex = annotationStartIndex + text.length + 1;
+        setReset(annotationEndIndex);
+    }
+
+    function triggerPopulateAnnotation(text) {
+        populateAnnotation({
+            ...getAnnotationState(),
+            text,
+        });
+    }
+
+    function isValidKey(key) {
+        let isValid = false;
+        if (1 === key.length) {
+            if (/[0-9a-zA-Z]/.test(key)) {
+                isValid = true;
+            }
+        }
+        return isValid;
+    }
+
+    function suggestAnnotations(searchTerm = '') {
+        if (!searchTerm.length) {
+            return;
+        }
+        let suggestions = [];
+        if ('tag' === isAnnotating) {
+            suggestions = tags.filter((a) => a.includes(searchTerm));
+        } else if ('mention' === isAnnotating) {
+            suggestions = mentions.filter((a) => a.startsWith(searchTerm));
+        }
+        setSuggestedAnnotations(suggestions);
+    }
+
+    function listenForTab(e) {
+        const { key } = e;
+        const { suggestedAnnotations } = getAnnotationState();
+
+        if (isAnnotating) {
+            switch (key) {
+                case 'Tab':
+                    e.preventDefault();
+                    triggerPopulateAnnotation(suggestedAnnotations[0]);
+                    break;
+            }
+        }
+    }
+
+    function listenForAnnotation(e) {
+        const { key } = e;
+        const { searchTerm } = getAnnotationState();
+
+        if (isAnnotating) {
+            if (isValidKey(key)) {
+                setSearchTerm(`${searchTerm}${key}`);
+            } else {
+                switch (key) {
+                    case 'Backspace':
+                        if (searchTerm.length) {
+                            setSearchTerm(searchTerm.slice(0, -1));
+                        } else {
+                            setReset();
+                        }
+                        break;
+                    case 'Escape':
+                    case ' ':
+                        setReset();
+                        break;
+                }
+            }
+        }
+
+        if ('#' === key) {
+            setIsAnnotatingStart(true);
+            setIsAnnotating('tag');
+        }
+
+        if ('@' === key) {
+            setIsAnnotatingStart(true);
+            setIsAnnotating('mention');
+        }
+    }
+
+    return (
+        <div className="py-12">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="grid grid-cols-2 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="p-6 bg-white border-b border-gray-200">
+                        <form name="entryForm" onSubmit={handleSubmit}>
+                            <div className="flex flex-col">
+                                <div className="mb-4">
+                                    <label>Date</label>
+                                    <input
+                                        className="w-full p-4 border-gray-200"
+                                        label="date"
+                                        name="date"
+                                        onChange={e => setData('date', e?.target?.value)}
+                                        type="date"
+                                        value={data.date}
+                                    />
+                                    <span className="text-red-600">
+                                        {formErrors.date}
+                                    </span>
+                                </div>
+
+                                <div className="mb-4">
+                                    <label>Tempo</label>
+                                    <input
+                                        className="w-full p-4 border-gray-200"
+                                        label="Tempo"
+                                        name="tempo"
+                                        onChange={e => setData('tempo', e?.target?.value)}
+                                        type="number"
+                                        value={data.tempo}
+                                    />
+                                    <span className="text-red-600">
+                                        {formErrors.tempo}
+                                    </span>
+                                </div>
+
+                                <div className="mb-0">
+                                    <label>Entry</label>
+                                    <textarea
+                                        className="w-full h-96 p-4 border-gray-200"
+                                        errors={formErrors.entry}
+                                        label="entry"
+                                        name="entry"
+                                        onChange={e => setData('entry', e?.target?.value)}
+                                        onKeyDown={e => listenForTab(e)}
+                                        onKeyUp={e => listenForAnnotation(e)}
+                                        ref={inputRef}
+                                        type="text"
+                                        value={data.entry}
+                                    />
+                                    <span className="text-red-600">
+                                        {formErrors.entry}
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div className="mt-4 flex justify-end">
+                                <button
+                                    type="submit"
+                                    className="px-6 py-2 font-bold text-white bg-blue-500 rounded"
+                                >
+                                    {isExistingEntry ? 'Update' : 'Log'}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="p-6 bg-white border-b border-gray-200">
+                        <div className="flex flex-col">
+                            <div className="mb-4">
+                                <label>Suggested</label>
+                                <div className={`
+                                    p-4 border border-gray-200 ${(isAnnotating) ? 'bg-green-50' : 'h-40'}
+                                `}>
+                                    {
+                                        suggestedAnnotations.map((annotation, i) => {
+                                            return <AutoAnnotation
+                                                callback={triggerPopulateAnnotation}
+                                                key={i}
+                                                type="button"
+                                            >{annotation}</AutoAnnotation>
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- resources/js/Pages/Entries/Edit.jsx
+++ resources/js/Pages/Entries/Edit.jsx
@@ -1,106 +1,22 @@
+import Form from '@/Components/Entry/Form';
 import Authenticated from '@/Layouts/Authenticated';
-import { Head, useForm } from '@inertiajs/inertia-react';
+import { Head } from '@inertiajs/inertia-react';
 import React from 'react';
 
 export default function Edit({ auth, entry, errors, mentions, tags }) {
-    const { id, date, tempo, entry: body } = entry;
-    const initialState = {
-        date,
-        tempo,
-        entry: body,
-    };
-    const { data, errors: formErrors, put, setData } = useForm(initialState);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        put(route('entries.update', id), {
-            onSuccess: () => {
-                // @todo: flash notify
-                console.log('Entry updated');
-            },
-        });
-    }
-
     return (
         <Authenticated
             auth={auth}
             errors={errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Entry</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Update</h2>}
         >
-            <Head title="Entry" />
+            <Head title="Update Entry" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            <form name="createForm" onSubmit={handleSubmit}>
-                                <div className="flex flex-col">
-                                    <div className="mb-4">
-                                        <label>Date</label>
-                                        <input
-                                            type="date"
-                                            className="w-full p-4 border-gray-200"
-                                            label="date"
-                                            name="date"
-                                            value={data.date}
-                                            onChange={(e) =>
-                                                setData("date", e.target.value)
-                                            }
-                                        />
-                                        <span className="text-red-600">
-                                            {formErrors.date}
-                                        </span>
-                                    </div>
-
-                                    <div className="mb-4">
-                                        <label>Tempo</label>
-                                        <input
-                                            type="number"
-                                            className="w-full p-4 border-gray-200"
-                                            label="Tempo"
-                                            name="tempo"
-                                            value={data.tempo}
-                                            onChange={(e) =>
-                                                setData("tempo", e.target.value)
-                                            }
-                                        />
-                                        <span className="text-red-600">
-                                            {formErrors.tempo}
-                                        </span>
-                                    </div>
-
-                                    <div className="mb-0">
-                                        <label>Entry</label>
-                                        <textarea
-                                            type="text"
-                                            className="w-full h-96 p-4 border-gray-200"
-                                            label="entry"
-                                            name="entry"
-                                            errors={formErrors.entry}
-                                            value={data.entry}
-                                            onChange={(e) =>
-                                                setData("entry", e.target.value)
-                                            }
-                                        />
-                                        <span className="text-red-600">
-                                            {formErrors.entry}
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className="mt-4 flex justify-end">
-                                    <button
-                                        type="submit"
-                                        className="px-6 py-2 font-bold text-white bg-blue-500 rounded"
-                                    >
-                                        Update
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Form
+                dbEntry={entry}
+                mentions={mentions}
+                tags={tags}
+            />
         </Authenticated>
     );
 }
