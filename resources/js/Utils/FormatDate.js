a9fc998bc788f3f0b5cee1d549fd7bbc6dd00be8
Steve Steele <steven.harville.steele@gmail.com>
2022-07-22 15:17:31 -0500

    Add 0-padding to format date

--- resources/js/Utils/FormatDate.js
+++ resources/js/Utils/FormatDate.js
@@ -1,5 +1,9 @@
 export default function FormatDate(date = '') {
     const dateObj = new Date(date);
     const offsetDateObj = new Date(dateObj.getTime() - dateObj.getTimezoneOffset() * -60000);
-    return offsetDateObj.toLocaleDateString();
+    return offsetDateObj.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    });
 }
