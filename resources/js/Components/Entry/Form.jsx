ca7104a139fc3260160e172d57ca42e92422d607
Steve Steele <steven.harville.steele@gmail.com>
2022-07-22 14:24:20 -0500

    Update mobile style

--- resources/js/Components/Entry/Form.jsx
+++ resources/js/Components/Entry/Form.jsx
@@ -181,7 +181,7 @@ export default function Form({ dbEntry = {}, mentions, tags }) {
     return (
         <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="grid grid-cols-2 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div className="grid grid-cols-1 md:grid-cols-2 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div className="p-6 bg-white border-b border-gray-200">
                         <form name="entryForm" onSubmit={handleSubmit}>
                             <div className="flex flex-col">
