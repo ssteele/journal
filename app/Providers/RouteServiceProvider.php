f287b6c69fbacc2c5c93f22bc9bc9a56590caeeb
Steve Steele <steven.harville.steele@gmail.com>
2022-07-25 09:53:57 -0500

    Update home page (redirected here after login)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/entries/create';
+    public const HOME = '/entries';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
