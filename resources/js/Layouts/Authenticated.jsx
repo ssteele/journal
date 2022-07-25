bb94f88d49a0420b79b54dc0755afd1e1f44d020
Steve Steele <steven.harville.steele@gmail.com>
2022-07-25 14:01:34 -0500

    Add back arrow navigation

--- resources/js/Layouts/Authenticated.jsx
+++ resources/js/Layouts/Authenticated.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
+import BackArrow from '@/Components/BackArrow';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
@@ -7,6 +8,7 @@ import { Link } from '@inertiajs/inertia-react';
 
 export default function Authenticated({ auth, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+    const doRenderBackArrow = ['entries.create', 'entries.show', 'entries.edit', 'create-upload'].includes(route().current());
 
     return (
         <div className="min-h-screen bg-gray-100">
@@ -14,11 +16,21 @@ export default function Authenticated({ auth, header, children }) {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
-                            <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto text-gray-500" />
-                                </Link>
-                            </div>
+                            { !doRenderBackArrow && (
+                                <div className="shrink-0 flex items-center">
+                                    <Link href="/">
+                                        <ApplicationLogo className="block h-9 w-auto text-gray-500" />
+                                    </Link>
+                                </div>
+                            )}
+
+                            { doRenderBackArrow && (
+                                <div className="shrink-0 flex items-center">
+                                    <Link onClick={() => history.back()}>
+                                        <BackArrow className="block h-9 w-auto text-gray-600" />
+                                    </Link>
+                                </div>
+                            )}
 
                             <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink href={route('entries.index')} active={route().current('entries.index')}>
