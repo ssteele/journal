82912e4aafd10964c1ce12d7884d9944def7ded8
Steve Steele <steven.harville.steele@gmail.com>
2022-08-01 06:12:40 -0500

    POC lazy loading the full timeline

--- resources/js/Pages/Tags/Show.jsx
+++ resources/js/Pages/Tags/Show.jsx
@@ -2,11 +2,14 @@ import Timeline from '@/Components/Annotation/Timeline';
 import Authenticated from '@/Layouts/Authenticated';
 import { getTimelineFrequency, getTimelineYears } from '@/Utils/Timeline';
 import { Head } from '@inertiajs/inertia-react';
-import React from 'react';
+import React, { Suspense } from "react";
+
+const FullTimeline = React.lazy(() => import('../../Components/Annotation/Timeline'));
 
 export default function Show({ auth, errors, tag, timeline }) {
     const timelineFrequency = getTimelineFrequency(timeline);
     const timelineYears = getTimelineYears(timelineFrequency);
+    const mostRecentTimelineYear = [timelineYears[timelineYears.length - 1]];
 
     return (
         <Authenticated
@@ -23,10 +26,17 @@ export default function Show({ auth, errors, tag, timeline }) {
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div className="mt-12 pb-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div className="p-6 bg-white">
-                        <Timeline
-                            timelineFrequency={timelineFrequency}
-                            timelineYears={timelineYears}
-                        ></Timeline>
+                        <Suspense fallback={
+                            <Timeline
+                                timelineFrequency={timelineFrequency}
+                                timelineYears={mostRecentTimelineYear}
+                            ></Timeline>
+                        }>
+                            <FullTimeline
+                                timelineFrequency={timelineFrequency}
+                                timelineYears={timelineYears}
+                            ></FullTimeline>
+                        </Suspense>
                     </div>
                 </div>
             </div>
