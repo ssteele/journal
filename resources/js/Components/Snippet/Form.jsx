49c2aa70d8bdf3367007a491a6b9c2236351f1cc
Steve Steele <steven.harville.steele@gmail.com>
2022-12-17 08:34:20 -0600

    Move fields around

--- resources/js/Components/Snippet/Form.jsx
+++ resources/js/Components/Snippet/Form.jsx
@@ -121,7 +121,49 @@ export default function Form({ dbSnippet = {}, tags = [] }) {
                 <div className="grid grid-cols-1 pb-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div className="px-6 bg-white">
                         <div className="mt-6">
+                            <label>Description</label>
+                            <input
+                                className="w-full p-4 border border-gray-200"
+                                label="description"
+                                name="description"
+                                onChange={e => setData('description', e?.target?.value)}
+                                type="description"
+                                value={data.description}
+                            />
+                            <span className="text-red-600">
+                                {errors.description}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="px-6 bg-white">
+                        <div className="mt-6">
                             <div className="grid grid-cols-3 pb-4 bg-white">
+                                <div>
+                                    <select
+                                        // className="border-gray-200 focus:border-indigo-200 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-xl shadow-sm sm:text-sm"
+                                        className="border border-gray-200"
+                                        // className="w-full p-4 border border-gray-200"
+                                        // name={name}
+                                        // required={required}
+                                        // id={id}
+                                        // onChange={onChange}
+                                    >
+                                        <option>Hello</option>
+                                        <option>There</option>
+                                        {/*
+                                        <option>{placeholder}</option>
+                                        {options.map((option, index) => {
+                                            return (
+                                                <option key={index} value={option.value}>
+                                                    {option.label}
+                                                </option>
+                                            );
+                                        })}
+                                        */}
+                                    </select>
+                                </div>
+
                                 <div className="grid grid-cols-2 bg-white">
                                     <div className="p-2">
                                         <label>
@@ -148,7 +190,7 @@ export default function Form({ dbSnippet = {}, tags = [] }) {
                                     </div>
                                 </div>
 
-                                <div className="p-2">
+                                <div className="p-2 justify-self-end">
                                     <label
                                         className="mr-2"
                                         // onClick="toggleAllDays"  // @todo
@@ -170,49 +212,7 @@ export default function Form({ dbSnippet = {}, tags = [] }) {
                                         )
                                     })}
                                 </div>
-
-                                <div className="justify-self-end">
-                                    <select
-                                        // className="border-gray-200 focus:border-indigo-200 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-xl shadow-sm sm:text-sm"
-                                        className="border border-gray-200"
-                                        // className="w-full p-4 border border-gray-200"
-                                        // name={name}
-                                        // required={required}
-                                        // id={id}
-                                        // onChange={onChange}
-                                    >
-                                        <option>Hello</option>
-                                        <option>There</option>
-                                        {/*
-                                        <option>{placeholder}</option>
-                                        {options.map((option, index) => {
-                                            return (
-                                                <option key={index} value={option.value}>
-                                                    {option.label}
-                                                </option>
-                                            );
-                                        })}
-                                        */}
-                                    </select>
-                                </div>
                             </div>
-                        </div>
-                    </div>
-
-                    <div className="px-6 bg-white">
-                        <div className="mt-6">
-                            <label>Description</label>
-                            <input
-                                className="w-full p-4 border border-gray-200"
-                                label="description"
-                                name="description"
-                                onChange={e => setData('description', e?.target?.value)}
-                                type="description"
-                                value={data.description}
-                            />
-                            <span className="text-red-600">
-                                {errors.description}
-                            </span>
                         </div>
                     </div>
 
