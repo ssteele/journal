bbf7b8c765547bc0f699964a5c3b52fbcbd98e62
Steve Steele <steven.harville.steele@gmail.com>
2023-02-04 12:19:05 -0600

    Add postcss-for package

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
+        "postcss-for": {},
         "postcss-css-variables": {},
         "postcss-color-function": {},
         "postcss-color-mix": {},
