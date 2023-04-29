cd2d4865faafe3bab8daf5bf7471950701ddb88f
Steve Steele <steven.harville.steele@gmail.com>
2023-04-29 15:09:02 -0500

    Flesh out edit panel

--- resources/js/Components/Annotation/Detail/EditPanel.jsx
+++ resources/js/Components/Annotation/Detail/EditPanel.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function EditPanel({ annotation, annotationType }) {
     return (
-        <div className="mt-6">
-            <button>{ annotation?.name }</button>
-        </div>
+        <>
+            <div className="mt-6">
+                <label>Name</label>
+                <div className="p-4 border border-gray-100 bg-gray-100 cursor-pointer">{ annotation?.name }</div>
+            </div>
+
+            <div className="mt-6">
+                <label>Nest Under</label>
+                <div className="p-4 border border-gray-100 bg-gray-100 cursor-pointer">{ annotation?.name }</div>
+            </div>
+        </>
     );
 }
