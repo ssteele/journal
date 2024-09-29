78d69853a2049a2e4d740d07a5d6a50020705252
Steve Steele <steven.harville.steele@gmail.com>
2024-09-29 14:44:39 -0500

    Add site favicon links/assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/imgs/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/imgs/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/imgs/favicon-16x16.png">
+        <link rel="icon" href="/imgs/favicon.ico">
 
         <!-- Scripts -->
         @routes
