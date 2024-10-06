618fb146fd484914d3cd8458bef806735a4a2331
Steve Steele <steven.harville.steele@gmail.com>
2024-10-06 06:10:32 -0500

    Remove console log

--- resources/js/Pages/Snippets/Index.jsx
+++ resources/js/Pages/Snippets/Index.jsx
@@ -34,7 +34,6 @@ export default function Index({
                 return dbMentionSnippets;
         
             default:
-                console.log('SHS dbEntrySnippets:', dbEntrySnippets); // @debug
                 return dbEntrySnippets;
         }
     }
