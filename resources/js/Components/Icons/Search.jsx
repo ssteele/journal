b3ee86c5d2d5da0f1a0eace22cbd442fb887d9c9
Steve Steele <steven.harville.steele@gmail.com>
2025-04-20 16:45:41 -0500

    Add searching indicator

--- resources/js/Components/Icons/Search.jsx
+++ resources/js/Components/Icons/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SearchIcon({ className, strokeColor = 'black' }) {
   return (
     <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
-      <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke={strokeColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke={strokeColor} strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   );
 }
--- resources/js/Pages/Entries/Search.jsx
+++ resources/js/Pages/Entries/Search.jsx
@@ -1,18 +1,23 @@
 import SearchIcon from '@/Components/Icons/Search';
+import LoadingSpinner from '@/Components/LoadingSpinner';
 import Authenticated from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-react';
 import React, { useState } from 'react';
 
 export default function Search({ auth, errors: authErrors }) {
   const [searchTerm, setSearchTerm] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
 
   function searchEntries(e) {
     const term = e?.target?.value;
 
-    if ('Enter' === e.key && term.length) {
+    if ('Enter' === e.key) {
       setSearchTerm(term);
-      console.log('SHS searchTerm:', searchTerm); // @debug
-      // @todo: search
+      if (term.length) {
+        setIsSearching(true);
+        // @todo: search
+        // setIsSearching(false);
+      }
     }
   }
 
@@ -43,14 +48,31 @@ export default function Search({ auth, errors: authErrors }) {
 
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            {!searchTerm?.length && (
-              <div className="grid grid-cols-1 justify-items-center gap-8 px-6 py-12 bg-white border-b border-gray-200">
-                <SearchIcon className="block h-36 w-auto" strokeColor="#4b5563" />
-                <span className="text-gray-500">No search term entered</span>
-              </div>
-            )}
-          </div>
+          {isSearching && (
+            <div className="grid content-center justify-items-center h-96 p-6 bg-white border-b border-gray-200">
+              <LoadingSpinner />
+            </div>
+          )}
+
+          {!isSearching && (
+            <div
+              className={`
+                ${searchTerm?.length ? 'grid grid-cols-1 md:grid-cols-2' : ''}
+                bg-white
+                overflow-hidden
+                shadow-sm
+                sm:rounded-lg
+              `}
+            >
+
+              {!searchTerm?.length && (
+                <div className="grid grid-cols-1 content-center justify-items-center gap-8 h-96 p-6 bg-white border-b border-gray-200">
+                  <SearchIcon className="block h-36 w-auto" strokeColor="#4b5563" />
+                  <span className="text-gray-500">No search term entered</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </Authenticated>
