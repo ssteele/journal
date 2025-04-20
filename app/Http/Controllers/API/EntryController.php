55dac75ab0573f1602d3629a203a5e3c81960cb7
Steve Steele <steven.harville.steele@gmail.com>
2025-04-20 18:05:23 -0500

    Add entry search API endpoint

--- app/Http/Controllers/API/EntryController.php
+++ app/Http/Controllers/API/EntryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Entry;
 use App\Repositories\EntryRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EntryController extends Controller
 {
@@ -56,6 +57,36 @@ class EntryController extends Controller
         try {
             $entries = Entry::whereIn('id', $ids)
                 ->where('user_id', $user->id)
+                ->get();
+            if ($entries->count() > 0) {
+                return response()->json($entries);
+            }
+            throw new \Exception('No entries found');
+        } catch (\Exception $e) {
+            return response('Unable to get entries', 422);
+        }
+    }
+
+    /**
+     * Search entries by search term.
+     *
+     * @param  string  $searchTerm
+     * @return \Illuminate\Http\JsonResponse 
+     */
+    public function search($searchTerm)
+    {
+        $user = \Auth::user();
+        try {
+            // $entries = Entry::query()
+                // ->latest()
+                // ->select(['id', 'first_name', 'last_name', 'email', 'company', 'created_at'])
+                // ->where(function (Builder $subQuery) use ($searchTerm) {
+                //     $subQuery->where('entry', 'like', '%' . $searchTerm . '%');
+                // });
+
+            $entries = DB::table('entries')
+                ->where('user_id', $user->id)
+                ->where('entry', 'like', '%' . $searchTerm . '%')
                 ->get();
             if ($entries->count() > 0) {
                 return response()->json($entries);
